faadf6772a5dfe16417617531ddc1d1791980b0c
DonAzi <savaSh-00@mail.ru>
2021-11-04 16:51:14 +0000

    new update
    board been iterated {loop
    }

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
+let cardsNode = document.getElementById("boardId");
+for (let cells = 1; cells < 76; cells++) {
+  let theDiv = document.createElement("div");
+  cardsNode.appendChild(theDiv);
+  theDiv.classList.add("stylish");
 
+  theDiv.innerHTML = cells;
+}
+
+let theButtons = document
+  .getElementById("buttonsId")
+  .addEventListener("click", function () {
+    let randomNum = Math.floor(Math.random() * 76);
+
+    let theCells = document.querySelectorAll(".stylish");
+    theCells[randomNum].classList.add("activeColor");
+    console.log(randomNum);
+  });
